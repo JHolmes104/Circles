b7593b0c86303fc520434dc30f0460b5ed10c86d
JHolmes104 <jaconholmes10@icloud.com>
2023-10-20 11:27:31 +0100

    Now creates 100 circles with random sizes speeds and colours.

--- Circles/main.cpp
+++ Circles/main.cpp
@@ -23,37 +23,61 @@ int main()
 	int r = 20;
 
 	srand(time(0));
-	int x = rand() % (gScreenWidth - (r * 2));
-	int y = rand() % (gScreenHeight - (r * 2));
+	int circleData[100][8];
+	for (int i = 0; i < 100; i++)
+	{
+		//Position
+		circleData[i][0] = rand() % (gScreenWidth - (r * 2));
+		circleData[i][1] = rand() % (gScreenHeight - (r * 2));
 
-	int xDif = 2;
-	int yDif = 3;
+		//Speed control;
+		circleData[i][2] = rand() % 10 + 1;
+		circleData[i][3] = rand() % 10 + 1;
 
+		//Colour values.
+		circleData[i][4] = rand() % 256;
+		circleData[i][5] = rand() % 256;
+		circleData[i][6] = rand() % 256;
+		
+		//Radius
+		circleData[i][7] = rand() % 30 + 11;
+	}
+
+
+	circleData[0][0] = rand() % (gScreenWidth - (r * 2));
+	circleData[0][1] = rand() % (gScreenHeight - (r * 2));
+
+	srand(time(0));
 	while(UpdateFramework())
 	{
-		// Draws a circle at 100,200 with radius 20
-		DrawCircle(x, y, r);
-		x += xDif;
-		y += yDif;
-
-		if (x > gScreenWidth - (r * 2) || x < 0)
+		srand(time(0));
+		for (int i = 0; i < 100; i++)
 		{
-			xDif = -xDif;
+			// Draws a circle at 100,200 with radius 20
+			DrawCircle(circleData[i][0], circleData[i][1], circleData[i][7]);
+			ChangeColour(circleData[i][4], circleData[i][5], circleData[i][6], 255);
+			circleData[i][0] += circleData[i][2];
+			circleData[i][1] += circleData[i][3];
+
+			if (circleData[i][0] > gScreenWidth - (r * 2) || circleData[i][0] < 0)
+			{
+				circleData[i][2] = -circleData[i][2];
+			}
+
+			if (circleData[i][1] > gScreenHeight - (r * 2) || circleData[i][1] < 0)
+			{
+				circleData[i][3] = -circleData[i][3];
+			}
+
+			srand(time(0));
+			int changeDirChance = rand() % 100;
+
+			if (changeDirChance == 0)
+			{
+				circleData[i][2] = -circleData[i][2];
+				circleData[i][3] = -circleData[i][3];
+			}
 		}
-
-		if (y > gScreenHeight - (r * 2) || y < 0)
-		{
-			yDif = -yDif;
-		}
-
-			//srand(time(0));
-			//int changeDirChance = rand() % 100;
-
-			//if (changeDirChance == 0)
-			//{
-				//circleData[i][2] = -circleData[i][2];
-				//circleData[i][3] = -circleData[i][3];
-			//}
 	}
 
 	cout << "Bye bye" << endl;
